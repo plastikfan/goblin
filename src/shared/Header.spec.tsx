37392a2e094f0ba1acaf5d3287d82c83c2c5ccc0
Plastikfan <plastikfan@outlook.com>
2020-11-20 11:26:50 +0000

    pg144, Header 'renders correctly' test is failing

--- src/shared/Header.spec.tsx
+++ src/shared/Header.spec.tsx
@@ -1,5 +1,14 @@
-import React from 'react'
+import React from "react"
+import { Header } from "./Header"
+
+jest.mock("./CartWidget", () => ({
+  CartWidget: () => <div>Cart widget</div>
+}))
 
 describe("Header", () => {
-  it.todo("renders correctly")
+  it("renders correctly", () => {
+    const { container } = renderWithRouter(() => <Header />)
+    expect(container.innerHTML).toMatch("Goblin Store")
+    expect(container.innerHTML).toMatch("Cart Store")
+  })
 })
