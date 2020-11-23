e6784ee4e1a80fc42b1460c12d9353c4a8f7212b
Plastikfan <plastikfan@outlook.com>
2020-11-23 14:29:31 +0000

    fix failing test in Header (oops my bad)

--- src/shared/CartWidget/CartWidget.spec.tsx
+++ src/shared/CartWidget/CartWidget.spec.tsx
@@ -1,5 +1,30 @@
+import React from "react"
 import { CartWidget } from './CartWidget'
+import { fireEvent } from "@testing-library/react"
 
 describe("CartWidget", () => {
-  it.todo("renders correctly")
+  // pg145
+  //
+  it.todo("shows the amount of products in the cart")
+
+  // pg146
+  it("navigates to cart summary page on click", () => {
+
+    // getByRole is a selector (what-ever that means, css selector?
+    // see https://www.w3schools.com/cssref/css_selectors.asp)
+    // aria role: https://html.spec.whatwg.org/multipage/dom.html#wai-aria
+    // aria "link" role: https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/ARIA_Techniques/Using_the_link_role
+    //
+    const { getByRole, history } = renderWithRouter(() => (
+      <CartWidget />
+    ))
+
+    // so does this mean that the Cart Widget may contain mutipl elements
+    // but there is only a single item which contains the "link" role?
+    // Isnt this a very weak way to select an item; the "link" role is quite
+    // generic and common, so we could be selecting anything here.
+    //
+    fireEvent.click(getByRole("link"))
+    expect(history.location.pathname).toEqual("/cart")
+  })
 })
--- src/shared/Header.spec.tsx
+++ src/shared/Header.spec.tsx
@@ -1,3 +1,4 @@
+import { fireEvent } from "@testing-library/react"
 import React from "react"
 import { Header } from "./Header"
 
@@ -9,6 +10,18 @@ describe("Header", () => {
   it("renders correctly", () => {
     const { container } = renderWithRouter(() => <Header />)
     expect(container.innerHTML).toMatch("Goblin Store")
-    expect(container.innerHTML).toMatch("Cart Store")
+    expect(container.innerHTML).toMatch("Cart widget")
+  })
+
+  it("navigates to / on header title click", () => {
+    // getByText is defined in @testing-library/react, but it's not
+    // explicitly imported. I assume its a bit like how renderWithRouter
+    // is defined on global, which means it can be used without being imported.
+    //
+    const { getByText, history } = renderWithRouter(() => <Header />)
+    fireEvent.click(getByText("Goblin Store"))
+
+    // We expect that we end up on root url (pg144)
+    expect(history.location.pathname).toEqual("/")
   })
 })
